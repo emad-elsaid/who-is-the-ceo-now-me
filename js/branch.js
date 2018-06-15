1812d0d1c6f872e5835da51cbaa326df875b2d7c
Emad Elsaid <blazeeboy@gmail.com>
2018-06-15 16:23:49 +0200

    separate internet packages to be common

--- js/branch.js
+++ js/branch.js
@@ -6,7 +6,8 @@ Vue.component('company-branch', {
 
   data: function() {
     return {
-      selectedInternetLine: ''
+      selectedInternetLine: '',
+      internetPackages: internetPackages
     }
   },
 
@@ -24,13 +25,6 @@ var Branch = class {
     this.kilowattPrice = 0.60
     this.waterLiterPrice = 0.60
     this.internet = []
-    this.internetPackages = [
-      new Internet('WE upto 5Mb', 1024 * 5, 110),
-      new Internet('WE upto 15Mb', 1024 * 15, 150),
-      new Internet('WE upto 25Mb', 1024 * 25, 200),
-      new Internet('WE upto 50Mb', 1024 * 25, 250),
-      new Internet('WE upto 100Mb', 1024 * 100, 300)
-    ]
   }
 
   expenses() {
--- js/internet.js
+++ js/internet.js
@@ -5,3 +5,11 @@ var Internet = class {
     this.cost = cost
   }
 }
+
+var internetPackages = [
+  new Internet('WE upto 5Mb', 1024 * 5, 110),
+  new Internet('WE upto 15Mb', 1024 * 15, 150),
+  new Internet('WE upto 25Mb', 1024 * 25, 200),
+  new Internet('WE upto 50Mb', 1024 * 25, 250),
+  new Internet('WE upto 100Mb', 1024 * 100, 300)
+]
